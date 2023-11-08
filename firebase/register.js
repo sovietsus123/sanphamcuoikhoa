625e7deda110565cc6079fbf4fbdc544b8ef8a57
sovietsus123 <susmeme174@gmail.com>
2023-11-08 17:23:31 +0700

    sanpham

--- firebase/register.js
+++ firebase/register.js
@@ -1,51 +1,48 @@
-import { auth, db} from './config.js';
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
-import { collection, query, getDocs, addDoc, serverTimestamp, orderBy, onSnapshot, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import { auth, db } from "./config.js";
 
-const usernameElm = document.getElementById('username');
-const passwordElm = document.getElementById('password');
-const registerBtn = document.getElementById('register-btn');
+const usernameElm = document.getElementById("username");
+const passwordElm = document.getElementById("password");
+const registerBtn = document.getElementById("register-btn");
 
 const ref = collection(db, "user");
-const q = query(ref, orderBy('createdAt'));
-const addUser = async(user) => {
-    const email = usernameElm.value;
-    if(email.value !== ""){
-        await addDoc(ref, {
-            email: email,
-            friends:[],
-            createdAt: serverTimestamp(),
-        });
-    }
+const addUser = async (user) => {
+  console.log(user);
+  await addDoc(ref, {
+    email: user.email,
+    friends: [],
+    createdAt: serverTimestamp(),
+  });
+  window.location = "./login.html";
 };
 
 const handleRegister = () => {
-    const email = usernameElm.value;
-    const password = passwordElm.value;
+  const email = usernameElm.value;
+  const password = passwordElm.value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            const email = user.email;
-            const displayName = user.displayName;
-            const photoURL = user.photoURL;
-            const userProfile = {
-                email,
-                displayName,
-                photoURL
-            }
-            localStorage.setItem("currentUser", JSON.stringify(userProfile));
-            alert("Sign in successfully");
-            window.location = './login.html';
-            // Signed in 
-
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorCode);
-        });
-}
-
-registerBtn.addEventListener('click', addUser);
-registerBtn.addEventListener('click', handleRegister);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const email = user.email;
+      const displayName = user.displayName;
+      const photoURL = user.photoURL;
+      const userProfile = {
+        email,
+        displayName,
+        photoURL,
+      };
+      localStorage.setItem("currentUser", JSON.stringify(userProfile));
+      alert("Register is successfully");
+      addUser(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      alert(errorCode);
+    });
+};
+registerBtn.addEventListener("click", handleRegister);
